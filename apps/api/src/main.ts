fbbfd48fba0e1c1b512787e39a3a084748d65ed1
LeMizoo <tovoniaina.rahendrison@gmail.com>
2026-06-25 09:40:05 +0300

    fix: CORS wildcard

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,19 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://trans-bygagoos.vercel.app',
-      'https://trans-bygagoos-mobile.vercel.app',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
     credentials: true,
   });
   
   app.setGlobalPrefix('api/v1');
   
   await app.listen(process.env.PORT || 3000);
-  console.log('🚀 API démarrée sur http://localhost:3000');
+  console.log('🚀 API démarrée');
 }
 bootstrap();
