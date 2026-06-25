77e7f62782f45ea4f9c6bd105cd1d067844e651a
LeMizoo <tovoniaina.rahendrison@gmail.com>
2026-06-25 09:11:46 +0300

    fix: CORS pour Vercel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,24 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: '*',
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:5174',
+      'https://trans-bygagoos.vercel.app',
+      'https://trans-bygagoos-mobile.vercel.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
   
-  app.useGlobalPipes(new ValidationPipe({ 
-    transform: true,
-    whitelist: false,
-  }));
-  
   app.setGlobalPrefix('api/v1');
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
   console.log('🚀 API démarrée sur http://localhost:3000');
 }
 bootstrap();
