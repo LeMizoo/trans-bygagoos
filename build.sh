d505c28633abef200b860fa3c4f4542d202a5383
LeMizoo <tovoniaina.rahendrison@gmail.com>
2026-06-25 08:57:45 +0300

    fix: tsc au lieu de nest build

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 npm install
 cd apps/api
 npx prisma generate
-# Utiliser le chemin absolu vers nest
-/opt/render/project/src/node_modules/.bin/nest build
+# Compiler avec tsc au lieu de nest
+npx tsc -p tsconfig.build.json
