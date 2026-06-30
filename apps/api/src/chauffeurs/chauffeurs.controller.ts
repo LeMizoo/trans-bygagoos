0c4ccc6a6806f0a25a0ad5a229a2a4499a52cf4d
LeMizoo <tovoniaina.rahendrison@gmail.com>
2026-06-30 11:34:06 +0300

    fix: Ajout route dashboard/:id pour l'app mobile chauffeur

--- apps/api/src/chauffeurs/chauffeurs.controller.ts
+++ apps/api/src/chauffeurs/chauffeurs.controller.ts
@@ -5,11 +5,38 @@ import { ChauffeursService } from './chauffeurs.service';
 export class ChauffeursController {
   constructor(private readonly service: ChauffeursService) {}
 
-  @Get() findAll() { return this.service.findAll(); }
-  @Get('dashboard') getDashboard() { return { message: 'OK' }; }
-  @Get(':id') findOne(@Param('id') id: string) { return this.service.findOne(id); }
-  @Post() create(@Body() data: any) { return this.service.create(data); }
-  @Put(':id') update(@Param('id') id: string, @Body() data: any) { return this.service.update(id, data); }
-  @Delete(':id') delete(@Param('id') id: string) { return this.service.delete(id); }
-  @Post(':id/toggle-actif') toggleActif(@Param('id') id: string) { return this.service.toggleActif(id); }
+  @Get()
+  findAll() {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.service.findOne(id);
+  }
+
+  @Get(':id/dashboard')
+  getDashboard(@Param('id') id: string) {
+    return this.service.getDashboard(id);
+  }
+
+  @Post()
+  create(@Body() data: any) {
+    return this.service.create(data);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() data: any) {
+    return this.service.update(id, data);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.service.delete(id);
+  }
+
+  @Post(':id/toggle-actif')
+  toggleActif(@Param('id') id: string) {
+    return this.service.toggleActif(id);
+  }
 }
